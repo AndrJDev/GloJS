efe6bc85537a9d6bbaed4bfb5ab244570fd770b4
AndrJdev <andgggm@gmail.com>
2021-11-26 21:48:19 +0300

    lesson02-fix1

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = 'Название проекта';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 1000;
+let rollback = 50;
+let fullPrice = 50000;
+let adaptive = true;
 
-alert('Hello world');
-console.log('Привет');
+//Вывести в консоль тип данных значений переменных 
+//title, fullPrice, adaptive;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+// Вывести в консоль длину строки screens
+console.log(screens.length);
+
+// Вывести в консоль 
+//“Стоимость верстки экранов (screenPrice) рублей” 
+//“Стоимость разработки сайта (fullPrice) рублей”
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+// Привести строку screens к нижнему регистру 
+// разбить строку на массив, вывести массив в консоль
+console.log(screens.toLocaleLowerCase().split(", "));
+
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+console.log("Процент отката посреднику за работу " + (fullPrice * (rollback / 100)));

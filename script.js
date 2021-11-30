fd15b4d1dd54098860b4f531033900404689e751
AndreyScherbakov <and_re@mail.ru>
2021-11-30 14:41:38 +0300

    lesson03_fix1

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
 "use strict";
-
-let title = 'Название проекта';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 1000;
-let rollback = 10;
-let fullPrice = 50000;
-let adaptive = true;
-
-//Вывести в консоль тип данных значений переменных 
-//title, fullPrice, adaptive;
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-// Вывести в консоль длину строки screens
-console.log(screens.length);
-
-// Вывести в консоль 
-//“Стоимость верстки экранов (screenPrice) рублей” 
-//“Стоимость разработки сайта (fullPrice) рублей”
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
-
-// Привести строку screens к нижнему регистру 
-// разбить строку на массив, вывести массив в консоль
-console.log(screens.toLocaleLowerCase().split(", "));
-
-// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-console.log("Процент отката посреднику за работу " + (fullPrice * (rollback / 100)));
-
 // lesson03
 
-title = prompt('Как называется ваш проект?');
-screens = prompt('Какие типы экранов нужно разработать?');
-screenPrice = +prompt('Сколько будет стоить данная работа?');
-adaptive = confirm('Нужен ли адаптив на сайте?');
+let rollback = 10;
+let fullPrice;
 
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
+let title = prompt('Как называется ваш проект?', 'Калькулятор');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа?', 50000);
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Слайдер');
+let servicePrice1 = +prompt('Сколько это будет стоить?', 2000);
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'php mail');
+let servicePrice2 = +prompt('Сколько это будет стоить?', 3000);
 
 //Итоговая стоимость работы
 fullPrice = screenPrice + servicePrice1 + servicePrice2;

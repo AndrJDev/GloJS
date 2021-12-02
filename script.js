82bf12b9c349c461663ec0164689ee45c2b06893
AndreyScherbakov <and_re@mail.ru>
2021-12-02 17:37:21 +0300

    lesson05

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 "use strict";
-// lesson04
+// lesson05
 
-let title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки');
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-let screenPrice = +prompt('Сколько будет стоить данная работа?', 50000);
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Слайдер');
-let servicePrice1 = +prompt('Сколько это будет стоить?', 2000);
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'php mail');
-let servicePrice2 = +prompt('Сколько это будет стоить?', 3000);
-
+let title;
+let screens;
+let screenPrice;
+let adaptive;
 let rollback = 10;
 let fullPrice;
-
 let allServicePrices;
 let servicePercentPrice;
+
+let service1; 
+let service2;
+
+const isThisA_Number = function (enteredNumber) { 
+    return (!isNaN(parseFloat(enteredNumber)) && isFinite(enteredNumber));    
+}
+
+const asking = function () { 
+    title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки');
+    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+
+    do {
+        screenPrice = prompt('Сколько будет стоить данная работа?', 50000);                
+    }while (!isThisA_Number(screenPrice));    
+
+    screenPrice = parseFloat(screenPrice);
+    adaptive = confirm('Нужен ли адаптив на сайте?');
+}
+
+const getAllServicePrices = function () {
+    let sum = 0;
+    let price = 0;
+
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            service1 = prompt("Какой дополнительный тип услуги " + i  + " нужен?", 'Слайдер');
+        }else if (i === 1) {
+            service2 = prompt("Какой дополнительный тип услуги " + i  + " нужен?", 'php mail');
+        }                
+        do {
+            price = prompt('Сколько это будет стоить?', 2000);                        
+            console.log(price);
+        }while (!isThisA_Number(price));                            
+                
+        sum += parseFloat(price);       
+    }    
+    return sum;
+}
 
 //Подсчет итоговой стоимость минус сумма отката
 const getServicePercentPrices = function (price, rollback) {    
@@ -29,11 +61,7 @@ const getTitle = function (str) {
 }
 
 function getFullPrice() {
-    return screenPrice + servicePrice1 + servicePrice2;
-}
-
-const getAllServicePrices = function () {
-    return servicePrice1 + servicePrice2;
+    return screenPrice + allServicePrices;
 }
 
 const showTypeOf = function(variable) {
@@ -52,21 +80,22 @@ const getRollbackMessage = function(price) {
     }
 }
 
-//Сумма стоимости верстки и стоимости дополнительных услуг
-fullPrice = getFullPrice();
-
-//Откат посреднику
-servicePercentPrice  = getServicePercentPrices(fullPrice, rollback);
+asking();
 
 //Cумма всех дополнительных услуг
 allServicePrices = getAllServicePrices();
+//Сумма стоимости верстки и стоимости дополнительных услуг
+fullPrice = getFullPrice();
+//Откат посреднику
+servicePercentPrice  = getServicePercentPrices(fullPrice, rollback);
 
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
-console.log("Cкидка пользователю: " + getRollbackMessage(fullPrice));
+console.log("Сумма дополнительных услуг: " + allServicePrices);
 
+console.log("Cкидка пользователю: " + getRollbackMessage(fullPrice));
 console.log(getTitle(title));
 console.log("Стоимость за вычетом процента отката посреднику: " + servicePercentPrice);
 console.log(screens);

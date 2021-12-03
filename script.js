a487ca3227fb84ec760138b4b56ac217b3eb7bc4
AndreyScherbakov <and_re@mail.ru>
2021-12-03 03:33:31 +0300

    lesson06

--- script.js
+++ script.js
@@ -1,73 +1,40 @@
 "use strict";
-// lesson04
+// lesson06
+// Загадывание случайного числа от 1 до 100
 
-let title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки');
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-let screenPrice = +prompt('Сколько будет стоить данная работа?', 50000);
-let adaptive = confirm('Нужен ли адаптив на сайте?');
+let requiredNumber = 30;
 
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Слайдер');
-let servicePrice1 = +prompt('Сколько это будет стоить?', 2000);
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'php mail');
-let servicePrice2 = +prompt('Сколько это будет стоить?', 3000);
-
-let rollback = 10;
-let fullPrice;
-
-let allServicePrices;
-let servicePercentPrice;
-
-//Подсчет итоговой стоимость минус сумма отката
-const getServicePercentPrices = function (price, rollback) {    
-    let rollbackPercent = price * (rollback / 100);        
-    return Math.ceil(price - rollbackPercent);
+const isThisA_Number = function (num) { 		
+    return (!isNaN(parseFloat(num)) && isFinite(num));    
 }
 
-const getTitle = function (str) {
-    str = str.trim().toLowerCase();
-    return str[0].toUpperCase() + str.slice(1);
+const asking = function (message) {     
+    
+    let num = prompt(message);
+
+    if (typeof num === 'string') {				
+		num = parseFloat(num);
+		if (isNaN(num)) {
+            asking("Введите число!" );
+		}else {
+            if (num > requiredNumber) {        
+                asking("Загаданное число меньше. Попробуйте еще");
+            }
+            if (num < requiredNumber) {                
+                asking("Загаданное число меньше. Попробуйте еще");
+            }  
+            if (num === requiredNumber) {   
+                //Конец игры             
+                alert("Поздравляю, Вы угадали!!!");
+            }  
+        }            		
+	}else {
+		if (!isThisA_Number(num)) {
+            //Отмена
+            alert("Игра Окончена!" );			
+		}		
+	}
+    
 }
 
-function getFullPrice() {
-    return screenPrice + servicePrice1 + servicePrice2;
-}
-
-const getAllServicePrices = function () {
-    return servicePrice1 + servicePrice2;
-}
-
-const showTypeOf = function(variable) {
-    console.log(variable, typeof variable);
-}
-
-const getRollbackMessage = function(price) {
-    if (price >= 30000) {
-        return "Даем скидку в 10%";
-    }else if ((price >= 15000) && (price < 30000)) {
-        return "Даем скидку в 5%";
-    }else if ((price < 15000) && (price >= 0)) {
-        return "Скидка не предусмотрена";
-    }else if (price < 0) {
-        return "Что то пошло не так";
-    }
-}
-
-//Сумма стоимости верстки и стоимости дополнительных услуг
-fullPrice = getFullPrice();
-
-//Откат посреднику
-servicePercentPrice  = getServicePercentPrices(fullPrice, rollback);
-
-//Cумма всех дополнительных услуг
-allServicePrices = getAllServicePrices();
-
-showTypeOf(title);
-showTypeOf(screenPrice);
-showTypeOf(adaptive);
-
-console.log("Cкидка пользователю: " + getRollbackMessage(fullPrice));
-
-console.log(getTitle(title));
-console.log("Стоимость за вычетом процента отката посреднику: " + servicePercentPrice);
-console.log(screens);
-console.log(screens.split(', '));
+asking('Угадай число от 1 до 100');

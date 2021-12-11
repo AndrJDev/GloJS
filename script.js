13b947db0f5da79158a6156633807da223347d4d
AndreyScherbakov <and_re@mail.ru>
2021-12-11 22:03:33 +0300

    lesson10

--- script.js
+++ script.js
@@ -1,132 +1,113 @@
 "use strict";
-// lesson08
-// Рефакторинг. Подготовка для работы с версткой
+// lesson10
+// Работа с DOM
 
-const appData = {
-    title: '',
-    screens: [],
-    screenPrice: 0,
-    adaptive: true,
-    rollback : 10,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    allServicePrices: 0,
-    services: {},  
-    //Проверка - это число
-    isThisA_Number : function (enteredNumber) { 
-        return (!isNaN(parseFloat(enteredNumber)) && isFinite(enteredNumber));    
-    },   
-    //Проверка - это строка
-    isThisA_String : function (enteredString) { 
-        if (isNaN(Number(enteredString))) {            
-            return true;
-        }else {            
-            return false;
-        }        
-    },  
-    //Обработка заголовка
-    getTitle : function (str) {
-        str = str.trim().toLowerCase();
-        appData.title = str[0].toUpperCase() + str.slice(1);
-    },
-    // Вопросы пользователю
-    asking : function () { 
-        do {
-            appData.title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки');
-        }while (!appData.isThisA_String(appData.title));    
-        
-        for (let i = 0; i < 2; i++) { 
-            let name;
-            do {
-                name = prompt('Какие типы экранов нужно разработать?', 'Простые');
-            }while (!appData.isThisA_String(name));    
+// Восстановить порядок книг.
+const books = document.querySelectorAll('.books');
+const booksElements = document.querySelectorAll('.book');
+// console.log(books);
 
-            let price = 0;            
-            do {
-                price = prompt('Сколько будет стоить данная работа?', 25000);                
-            }while (!appData.isThisA_Number(price));    
+let book2 = booksElements[0];
+let book1 = booksElements[1];
+let book6 = booksElements[2];
+let book4 =  booksElements[3];
+let book3 =  booksElements[4];
+let book5 =  booksElements[5];
 
-            appData.screens.push({id: i, name: name, price: price});
-        }        
+books[0].append(book1); 
+books[0].append(book2); 
+books[0].append(book3); 
+books[0].append(book4); 
+books[0].append(book5); 
+books[0].append(book6); 
 
-        // Доп. услуги
-        for (let i = 0; i < 2; i++) {
-            let name;
-            do {
-                name = prompt("Какой дополнительный тип услуги нужен?", 'Слайдер');
-            }while (!appData.isThisA_String(name));    
+// Заменить картинку заднего фона на другую из папки image
+let bodyNode = document.getElementsByTagName('body')[0];
+function changeBgImg(){
+    bodyNode.style.backgroundImage = "url('/image/you-dont-know-js.jpg')";
+}
+changeBgImg();
 
-            let price = 0;            
+// Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
+// console.dir(book3);
+// console.dir(book3.innerHTML);
 
-            do {
-                price = prompt('Сколько это будет стоить?', 2000);                                        
-            }while (!appData.isThisA_Number(price));                            
-            
-            appData.services[name] = parseFloat(price);            
-        }   
-    
-        appData.screenPrice = parseFloat(appData.screenPrice);
-        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-    },
-    addPrices : function () {         
-        for (let screen of appData.screens) {
-            appData.screenPrice += +screen.price;            
-        }
-        // Сумма доп. услуг
-        for (let key in appData.services) {
-            appData.allServicePrices += appData.services[key];
-        }
-    },
-    // Переопределение свойств
-    propertyOverrides : function () {         
-        appData.getTitle(appData.title);        
-        //Сумма стоимости верстки и стоимости дополнительных услуг
-        appData.getFullPrice();
-        //Откат посреднику
-        appData.getServicePercentPrices(appData.fullPrice, appData.rollback);       
-    },
-    //Подсчет итоговой стоимость минус сумма отката
-    getServicePercentPrices : function (price, rollback) {    
-        let rollbackPercent = price * (rollback / 100);        
-        return Math.ceil(price - rollbackPercent);
-    },
-    //Подсчет итоговой стоимость минус сумма отката
-    getServicePercentPrices : function (price, rollback) {    
-        let rollbackPercent = price * (rollback / 100);        
-        appData.servicePercentPrice = Math.ceil(price - rollbackPercent);
-    },
-    // Сообщение о скидке
-    getRollbackMessage : function(price) {
-        if (price >= 30000) {
-            return "Даем скидку в 10%";
-        }else if ((price >= 15000) && (price < 30000)) {
-            return "Даем скидку в 5%";
-        }else if ((price < 15000) && (price >= 0)) {
-            return "Скидка не предусмотрена";
-        }else if (price < 0) {
-            return "Что то пошло не так";
-        }
-    },
-    getFullPrice() {
-        appData.fullPrice = appData.screenPrice + appData.allServicePrices;
-    },
-    //Вывод в консоль
-    logger : function () { 
-        console.log("Заголовок: " + appData.title);
-        console.log("Полная стоимость: " + appData.fullPrice);
-        console.log("Сумма дополнительных услуг: " + appData.allServicePrices);
-        console.log("Cкидка пользователю: " + appData.getRollbackMessage(appData.fullPrice));        
-        console.log("Стоимость за вычетом процента отката посреднику: " + appData.servicePercentPrice);
-                
-        console.log(appData.services);
-        console.log(appData.screens);                  
-    },    
-    start : function () {         
-        appData.asking();   
-        appData.addPrices();             
-        appData.propertyOverrides();
-        appData.logger(); 
-    },
-} 
+let html = book3.innerHTML;
+html = html.replace("Пропопипы", "Прототипы");
+book3.innerHTML = html;
 
-appData.start();
+// Удалить рекламу со страницы
+let reclam = document.querySelectorAll('.adv');
+reclam[0].style.display = 'none';
+
+// Восстановить порядок глав во второй и пятой книге 
+//(внимательно инспектируйте индексы элементов, поможет dev tools)
+
+// book2
+let book2_ul = book2.children[1];
+
+let book2_li_01 = book2.children[1].children[0];
+let book2_li_02 = book2.children[1].children[1];
+let book2_li_pC = book2.children[1].children[2];
+let book2_li_1= book2.children[1].children[3];
+let book2_li_4 = book2.children[1].children[4];
+let book2_li_5 = book2.children[1].children[5];
+let book2_li_2 = book2.children[1].children[6];
+let book2_li_pA = book2.children[1].children[7];
+let book2_li_3 = book2.children[1].children[8];
+let book2_li_pB = book2.children[1].children[9];
+let book2_li_pD = book2.children[1].children[10];
+
+book2_ul.append(book2_li_01); 
+book2_ul.append(book2_li_02);
+book2_ul.append(book2_li_1); 
+book2_ul.append(book2_li_2); 
+book2_ul.append(book2_li_3); 
+book2_ul.append(book2_li_4); 
+book2_ul.append(book2_li_5); 
+book2_ul.append(book2_li_pA); 
+book2_ul.append(book2_li_pB);
+book2_ul.append(book2_li_pC); 
+book2_ul.append(book2_li_pD);
+
+// book5
+let book5_ul = book5.children[1];
+
+let book5_li_01 = book5_ul.children[0];
+let book5_li_02 = book5_ul.children[1];
+let book5_li_4 = book5_ul.children[2];
+let book5_li_2= book5_ul.children[3];
+let book5_li_3 = book5_ul.children[4];
+let book5_li_pA = book5_ul.children[5];
+let book5_li_5 = book5_ul.children[6];
+let book5_li_6 = book5_ul.children[7];
+let book5_li_pB = book5_ul.children[8];
+let book5_li_1 = book5_ul.children[9];
+let book5_li_pC = book5_ul.children[10];
+
+book5_ul.append(book5_li_01); 
+book5_ul.append(book5_li_02); 
+
+book5_ul.append(book5_li_1); 
+book5_ul.append(book5_li_2); 
+book5_ul.append(book5_li_3); 
+book5_ul.append(book5_li_4); 
+book5_ul.append(book5_li_5); 
+book5_ul.append(book5_li_6); 
+
+book5_ul.append(book5_li_pA); 
+book5_ul.append(book5_li_pB); 
+book5_ul.append(book5_li_pC); 
+
+// в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+
+// скопировать гл 7 изменить ее название и встаить после нее 
+let book6_ul = book6.children[1];
+let book6_li_7 = book6_ul.children[8];
+let book6_li_8 = book6_li_7.cloneNode(true);
+
+let htmlText = book6_li_8.innerHTML;
+htmlText = htmlText.replace("Глава 7: Метапрограммирование", "Глава 8: За пределами ES6");
+book6_li_8.innerHTML = htmlText;
+
+book6_li_7.after(book6_li_8);
